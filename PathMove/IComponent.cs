05e98c5f059756f958a3811128cbad1c6320fe63
hanzzbrr <artur.galimsky@gmail.com>
2023-03-08 16:08:36 +0600

    Components

---
+++ PathMove/IComponent.cs
@@ -0,0 +1,10 @@
+namespace PathMove
+{
+    public interface IComponent
+    {
+        string Name { get; set; }
+        void AddComponent(IComponent component);
+        void RemoveComponent(IComponent component);
+        IComponent GetComponent<T>();
+    }
+}
